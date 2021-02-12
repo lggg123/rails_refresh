191d49935da30aae117e0389fe5e034eaf76626f
lggg123 <machinelearner2334@gmail.com>
2021-02-12 15:01:24 -0800

    postgres revision

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_02_02_180611) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.integer "birth_year"
